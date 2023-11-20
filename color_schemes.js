9db480cbcd90dac53b4ae6f1adace6b14082df2f
cryptobauhaus <77994061+cryptobauhaus@users.noreply.github.com>
2023-11-20 09:40:30 -0600

    Update color_schemes.js

--- color_schemes.js
+++ color_schemes.js
@@ -1,76 +1,51 @@
 if (what_color_scheme ===0 ) {
   var color_scheme = ['#D6476B','#EC8153','#AAC6C5','#90B8C1','#EFEDDE','#EF939E','#E0B25A','#F4B75D','#6D665E'];
-  var color_scheme_plot = ['#AAC6C5','#EFEDDE','#F4B75D','#6D665E'];
 } else if (what_color_scheme ===1)  {
   var color_scheme =  ['#ffe066','#f25f5c', '#50514f', '#70c1b3', '#247ba0', '#50514f', '#70c1b3', '#247ba0', '#50514f', '#70c1b3', '#247ba0', '#50514f', '#70c1b3', '#247ba0', '#50514f', '#70c1b3', '#247ba0'];
-  var color_scheme_plot =  ['#6292A6','#f25f5c', '#247ba0', '#727272','#727272'];
 } else if (what_color_scheme ===2)  {
   var color_scheme =  ['#DBD3D8','#EFF1F3', '#223843', '#D8B4A0', '#D77A61','#223843', '#D8B4A0', '#D77A61','#223843', '#D8B4A0', '#D77A61', '#247ba0'];
-  var color_scheme_plot =  ['#DBD3D8','#EFF1F3', '#223843', '#D8B4A0', '#D77A61'];
 } else if (what_color_scheme ===3 )  {
   var color_scheme =  ['#B4B8AB','#153243', '#284B63', '#F4F9E9', '#EEF0EB','#284B63', '#F4F9E9', '#EEF0EB','#C87845', '#5588AB'];
-  var color_scheme_plot =  ['#B4B8AB', '#EEF0EB','#B4B8AB', '#EEF0EB','#B4B8AB', '#EEF0EB','#B4B8AB', '#EEF0EB','#153243', '#284B63'];
 } else if (what_color_scheme ===4 ) {
   var color_scheme =  ['#5bc0eb','#e07a5f', '#3d405b', '#3d405b', '#81b29a', '#f2cc8f', '#f2cc8f'];
-var color_scheme_plot =  ['#5bc0eb','#e07a5f', '#81b29a'];
 } else if (what_color_scheme ===5 ) {
   var color_scheme =  ['#606c38', '#5E6945','#283618', '#fefae0', '#EAE2B1','#dda15e','#bc6c25','#fefae0', '#dda15e', '#fefae0', '#dda15e', '#744C30'];
-  var color_scheme_plot =  ['#606c38', '#fefae0', '#dda15e','#bc6c25','#606c38', '#fefae0', '#dda15e','#bc6c25', '#3A3F35'];
 } else if (what_color_scheme ===6) {
   var color_scheme =  ['#059FCE','#DC0278','#ECDB04','#222222','#222222','#222222'];
-  var color_scheme_plot =  ['#059FCE','#DC0278','#ECDB04','#222222'];  
 } else if (what_color_scheme ===7) {
   var color_scheme =  ['#627893', '#A1ADC1', '#DBE8F9', '#FA6220', '#FFF4E2', '#323A55'];
-  var color_scheme_plot =  ['#627893', '#DBE8F9', '#FFAA85'];
 } else if (what_color_scheme ===8) {
   var color_scheme =  ['#DBD1C7','#034C7D','#DD4089','#0AA5B4', '#091114'];
-  var color_scheme_plot =  ['#DBD1C7','#034C7D','#DD4089','#0AA5B4', '#091114'];
 } else if (what_color_scheme ===9) {
   var color_scheme =  ['#353531','#016FB9', '#EBB66D', '#F19074', '#ABCCE3', '#EBB66D', '#F19074', '#ABCCE3'];
-  var color_scheme_plot =  ['#353531','#FF9505', '#ABCCE3'];
 } else if (what_color_scheme ===10) {
   var color_scheme =  ['#960114','#97061C', '#D90131', '#C73B20', '#DA6E00', '#ED8E0A', '#EFB303', '#D9BA02', '#AB8E1C', '#BC7292', '#F57077', '#E69080', '#DDC4A3', '#D4CAC0', '#E9BB4B', '#CE8943', '#704425', '#3D2A11', '#2D83A0', '#02856E', '#676E00', '#504255', '#515779', '#43797E', '#2E294F', '#487684', '#99BECB'];
-  var color_scheme_plot =  ['#960114','#97061C', '#D90131', '#C73B20', '#DA6E00', '#ED8E0A', '#EFB303', '#D9BA02', '#AB8E1C', '#BC7292', '#F57077', '#E69080', '#DDC4A3', '#D4CAC0', '#E9BB4B', '#CE8943', '#704425', '#3D2A11', '#2D83A0', '#02856E', '#676E00', '#504255', '#515779', '#43797E', '#2E294F', '#487684', '#99BECB'];;
 } else if (what_color_scheme ===11) {
   var color_scheme =  ['#C4614C','#CBAA9F', '#C8C396', '#85B89D', '#D7B243'];
-  var color_scheme_plot =  ['#C4614C','#CBAA9F', '#C8C396', '#85B89D', '#D7B243'];
 } else if (what_color_scheme ===12) {
   var color_scheme =  ['#E02F18','#E45F33', '#E3A878', '#87ADA6', '#01697B','#0A486E'];
-  var color_scheme_plot =  ['#E02F18','#E45F33', '#E3A878', '#87ADA6', '#01697B','#0A486E'];
 } else if (what_color_scheme ===13) {
   var color_scheme =  ['#EEE4DB','#EEC7BE', '#F0A4D6', '#D39C24', '#F49F82','#F57D58','#99C1BB','#34666A'];
-  var color_scheme_plot =  ['#EEE4DB','#EEC7BE', '#F0A4D6', '#D39C24', '#F49F82','#F57D58','#99C1BB','#34666A'];
 } else if (what_color_scheme ===14) {
   var color_scheme =  ['#CC5851','#EA9CA1', '#8696AF', '#586E61', '#728685','#E5D8E5'];
-  var color_scheme_plot =  ['#CC5851','#EA9CA1', '#8696AF', '#586E61', '#728685','#E5D8E5'];
 } else if (what_color_scheme ===15) {
   var color_scheme =  ['#D0282E','#948101', '#B19715', '#515F22', '#322C1E'];
-  var color_scheme_plot =  ['#D0282E','#948101', '#B19715', '#515F22', '#322C1E'];
 } else if (what_color_scheme ===16) {
   var color_scheme =  ['#F2C1B6','#D27661', '#E3B162', '#655054', '#255A5F'];
-  var color_scheme_plot =  ['#F2C1B6','#D27661', '#E3B162', '#655054', '#255A5F'];
 } else if (what_color_scheme ===17) {
   var color_scheme =  ['#8FB8D4','#BBC8DD', '#F4763B', '#F0491F', '#F1BEC8'];
-  var color_scheme_plot =  ['#8FB8D4','#BBC8DD', '#F4763B', '#F0491F', '#F1BEC8'];
 } else if (what_color_scheme ===18) {
   var color_scheme =  ['#4E685C','#B95D43', '#EDDACA', '#D5B7A0', '#173324', '#A77850'];
-  var color_scheme_plot =  ['#4E685C','#B95D43', '#EDDACA', '#D5B7A0', '#173324', '#A77850'];
 } else if (what_color_scheme ===19) {
   var color_scheme =  ['#C4AD2B','#F1E398', '#1B5833', '#78AC58', '#ACC5A0'];
-  var color_scheme_plot =  ['#C4AD2B','#F1E398', '#1B5833', '#78AC58', '#ACC5A0'];
 } else if (what_color_scheme ===20) {
   var color_scheme =  ['#0061F4','#009769', '#F48B9D', '#E50131', '#F3C000', '#222222'];
-  var color_scheme_plot =  ['#0061F4','#009769', '#F48B9D', '#E50131', '#F3C000', '#222222'];
 }  else if (what_color_scheme ===21) {
   var color_scheme =  ['#9EC1C0','#047F89', '#BF6917', '#D07E4D', '#8E4539', '#705659'];
-  var color_scheme_plot =  ['#9EC1C0','#047F89', '#BF6917', '#D07E4D', '#8E4539', '#705659'];
 } else if (what_color_scheme ===22) {
   var color_scheme =  ['#F4E3C9','#F49C60', '#F27140', '#A84C3E', '#253045','#047F89'];
-  var color_scheme_plot =  ['#F4E3C9','#F49C60', '#F27140', '#A84C3E', '#253045','#047F89'];
 } else if (what_color_scheme ===23) {
   var color_scheme =  ['#036C78','#E37F2F', '#2E2E2E', '#B84C34', '#98C6AA','#836448', '#C3A206', '#A7AEB1'];
-  var color_scheme_plot =  ['#036C78','#E37F2F', '#2E2E2E', '#B84C34', '#98C6AA','#836448', '#C3A206', '#A7AEB1'];
 } else if (what_color_scheme ===24) {
   var color_scheme =  ['#E1DDD2','#D3CDC3', '#CBC5BB', '#C5BAAA', '#62604A','#84826F', '#ADB2A6', '#C0C1BA','#E7A679','#212115', '#3A3E42', '#728172', '#7A7E71','#5A5A4F', '#A09891', '#BFB8B1', '#C1BFB9', '#9EC1C0'];
-  var color_scheme_plot =  ['#E1DDD2','#D3CDC3', '#CBC5BB', '#C5BAAA', '#62604A','#84826F', '#ADB2A6', '#C0C1BA','#E7A679','#212115', '#3A3E42', '#728172', '#7A7E71','#5A5A4F', '#A09891', '#BFB8B1', '#C1BFB9', '#9EC1C0'];
 }
